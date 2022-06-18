4cf7676acec58f002452681e3f3817aa5e66cb74
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-06-18 16:38:50 -0300

    Adicionada rota para pegar tweets publicados.

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ app.use(express.json());
 app.use(cors());
 
 let usuarios = [];
+let tweets = [];
+
+function testaURL(urlAvatar) {
+    var regexp =
+    /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+    return regexp.test(urlAvatar);
+}
 
 app.post('/sign-up', (req, res) => {
     if (req.body.username !== "" && testaURL(req.body.avatar)) {
@@ -20,6 +27,12 @@ app.post('/sign-up', (req, res) => {
         res.send("OK");
     }
 });
+
+app.get('/tweets', (req, res) => {
+    let ultimosTweets = tweets.slice(-10);
+    res.send(ultimosTweets.reverse());
+})
+
 
 app.listen(5000, () => {
     console.log(chalk.bold.yellow('Ola o servidor está funcionando!!!!'));
