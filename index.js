f4d67c299193a671df5c6d4bdc494664a5d77058
Pedro <pedrohenriquebritooliveira@gmail.com>
2022-06-18 16:37:48 -0300

    Adicionada rota para fazer login.

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+let usuarios = [];
+
+app.post('/sign-up', (req, res) => {
+    if (req.body.username !== "" && testaURL(req.body.avatar)) {
+        usuarios = [
+            ...usuarios,
+            {
+                username: req.body.username,
+                avatar: req.body.avatar,
+            }
+        ];
+        res.send("OK");
+    }
+});
+
 app.listen(5000, () => {
     console.log(chalk.bold.yellow('Ola o servidor está funcionando!!!!'));
 });
